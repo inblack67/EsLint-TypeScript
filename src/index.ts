3b125d55321d986b96c142d8912110722495a960
Aman Bhardwaj <inblack>
2021-01-16 10:36:20 +0530

    cool

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import nodemon from 'http';
+
+const main = async () => {
+    const app = express();
+    const PORT = process.env.PORT || 5000;
+    app.listen( PORT, () => {
+        console.log( `Server started on port ${ PORT }` );
+    } );
+};
+
+main().catch( err => {
+    console.error( err );
+} );
